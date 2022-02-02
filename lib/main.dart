859c389f7584f49a33e57c7e6a50bd5408a21cd8
net <nati2mulugeta@gmail.com>
2022-02-02 10:04:35 +0300

    First session except for the stream'

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:getx_test/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,52 +17,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(secondary: Colors.purpleAccent)
       ),
-      home: const MyHomePage(),
+      home:  Home(),
     );
   }
 }
 
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('The Lion'),
-      ),
-      body: Center(child: 
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(onPressed: (){Get.to(const Welcome());}, child: const Text('Goto Home'))
-        ],
-      ),),
-    );
-  }
-}
-
-class Welcome extends StatelessWidget {
-  const Welcome({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Welcome'),
-      ),
-      body: Center(child: 
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(onPressed: (){Get.back();}, child: const Text('Goto back'))
-        ],
-      ),),
-    );
-  }
-}
---
+++ lib/screens/home.dart
@@ -0,0 +1,98 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_test/states/counterState.dart';
+
+
+class Home extends StatelessWidget {
+   Home({ Key? key }) : super(key: key);
+
+final CounterState counterState = Get.put(CounterState());
+
+goToNext() {
+  Get.to(const Other());
+  // Navigator.push(context, MaterialPageRoute(builder: (context) => Other()));
+}
+
+_showSnackBar() {
+  Get.snackbar("Hey There", "Snack is easy");
+}
+
+_showDialog()  {
+  Get.defaultDialog(title: "SimpleDialogue",content: const Text("Too Easy"));
+}
+
+_showBottomSheet() {
+  Get.bottomSheet(Container(
+    child: Wrap(children: [
+      ListTile(
+        leading: const Icon(Icons.music_note),
+        title:const Text('Music'),
+        onTap: () => {},
+      ),
+
+       ListTile(
+        leading:const Icon(Icons.videocam),
+        title:const Text('Video'),
+        onTap: () => {},
+      ),
+     const  SizedBox(height: 100,)
+      
+    ],)
+    ),
+    backgroundColor: Colors.white,
+    );
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Getx'),
+        centerTitle: true,
+      ),
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(onPressed: () => goToNext(), child: const Text("Go To Other")),
+          ElevatedButton(onPressed: () => _showSnackBar(), child: const Text("SnackBar")),
+          ElevatedButton(onPressed: () => _showDialog(), child: const Text("Dialog")),
+          ElevatedButton(onPressed: () => _showBottomSheet(), child: const Text("Bottom Sheet")),
+          const SizedBox(height: 50,),
+          Obx(
+            () =>  Text(
+              "State: " + counterState.count.string,
+              style: const TextStyle(fontSize: 30,
+              fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+      
+      ),
+       floatingActionButton: FloatingActionButton(
+        onPressed: () {
+         counterState.increment();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+class Other extends StatelessWidget {
+  const Other({Key? key}) : super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text("Other"),
+      ),
+    );
+  }
+}
